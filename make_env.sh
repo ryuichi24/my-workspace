7b8f6f01a15ff0c3f88c554b225fcdf5463d5c3e
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-10-11 14:34:04 +0900

    add init script

--- make_env.sh
+++ make_env.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 DEV_CONTAINER_DIR=./.devcontainer
-cp ${DEV_CONTAINER_DIR}/.env.example ${DEV_CONTAINER_DIR}/.env
+
+cp ${DEV_CONTAINER_DIR}/.env.example ${DEV_CONTAINER_DIR}/.env \
+    && rm ./make_env.sh
